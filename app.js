d672554eb8b8a8741b44bc335b71f4ade9761c1e
Cristian Paez <cpaezariza@gmail.com>
2024-08-09 17:36:35 -0500

    ..

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { documento,enviar } from "/modelo.js";
+import { documento,enviar } from "./modelo.js";
 async function llamar() {
   const datos =await fetch("http://localhost:3000/document");
   const parsiar =await datos.json();
@@ -62,4 +62,77 @@ $form.addEventListener("submit", validacion)
 
 llamar()
 
+const listar = async()=>{
+  const data=await solicitud('users');
+  const documentos=await solicitud('documento');
+  data.forEach(element => {
+    let documento=documentos.find((doc)=> doc.input_id===element.id);
+    $templete.querySelector("tr").id= `user_${element.id}`;
+    $templete.querySelector(".first_name").textContent=element.nombre;
+    $templete.querySelector(".last_name").textContent=element.apellido;
+    $templete.querySelector(".address").textContent=element.direccion;
+    $templete.querySelector(".type_id").textContent=documento.tipo_documento;
+    $templete.querySelector(".docuement").textContent=element.documento;
+    $templete.querySelector(".email").textContent=element.correo;
+    $templete.querySelector(".edit").setAttribute("data-id",element.id);
+    $templete.querySelector(".detele").setAttribute("data-id",element.id);
+    let clone =document.importNode($templete,true);
+    fragmento.appendChild(clone);
+  });
+  table.querySelector('tbody').appendChild(fragmento)
+}
+document.addEventListener("click",(e)=>{
+  if (e.target.matches(".edit")|| e.target.matches(".edit *")) {
+      element=e.target.matches(".edit")? e.target:e.target.parentNode;
+      edit(e,element);
+  }
+  if(e.target.matches(".delete")||e.target.marches(".delete *")){
 
+  }
+})
+
+// Formulario
+const envio_form=(datos)=>{
+  const{id,first_name,last_name,phone,address,type_id,docuement,email}=data;
+  user.value=id;
+  nombre.value=first_name;
+  apellido.value=last_name;
+  telofono.value=phone;
+  direccion.value=address;
+  tipo.value=type_id;
+  documento.value=docuement;
+  correo.value=address;
+  politicas.cheched=true;
+  send.removeAttribute("disable");
+
+
+}
+
+const createRow=async(datos)=>{
+  const documentos=await solicitud("documents");
+  const tr=tbody.insertRow(-1);
+  const tb_Nombre=tr.insertCell(0);
+  const tb_Apellido=tr.insertCell(1);
+  const tb_Direccion=tr.insertCell(2);
+  const tb_Tipo=tr.insertCell(3);
+  const tb_Documentio=tr.insertCell(4);
+  const tb_Email=tr.insertCell(5);
+  const tb_Botonera=tr.insertCell(6);
+  
+
+  tb_Nombre=data.$input_name;
+  tb_Apellido=data.$input_apellido;
+  tb_Direccion=data.$input_direccion;
+  tb_Tipo=data.$input_tipo;
+  tb_Email=data.$input_correo;
+  tb_Documentio=data.$input_document;
+
+  tb_Nombre.classList.add()
+}
+const tr= document.querySelector(`#user_${id}`);
+tr.querySelector(".first_name").textContent=first_name;
+tr.querySelector(".last_name").textContent=last_name_name;
+tr.querySelector(".address").textContent=address;
+tr.querySelector(".type_id").textContent=documento.find((doc)=>doc.id===data.type_id).name;
+tr.querySelector(".dni").textContent=dni;
+tr.querySelector(".email").textContent=email
--- modelo.js
+++ modelo.js
@@ -1,26 +1,43 @@
+
 export const documento=async(id)=>{
    const data=await fetch(`http://localhost:3000/document/${id}`);
    const dato= await data.json();
    return dato
 
 }
-export const enviar= async(datos) =>{
-    fetch('http://localhost:3000/user', {
+export const enviar= async datos =>{
+  const data=await fetch('http://localhost:3000/user', {
     method: 'POST',
     body: JSON.stringify(datos),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 }
-export const listar=async()=>{
-  const data=await fetch("http://localhost:3000/user");
-  const dato=await data.json();
-  return dato
+
+
+const edit=(event,elemnt)=>{
+  enviar(`users/${elemnt.dataset.id}`,{
+    method:"PATCH",
+    headers:{
+      "Content-type":"application/json; charsert=UTF-8",
+    },
+  }).then((data)=>{
+    loadForm(data);
+    toggleModal();
+  })
 }
-export const eliminar=async(id)=>{
-  const data= await fetch(`http://localhost:3000/user/${id}`)
-  
+const deletedata=(event,elemnt)=>{
+  const tr=elemnt.parentNode.parentNode.parentNode;
+  if (confirm("¿Desea eliminar el registro?")) {
+    enviar(`users/${elemnt.dataset.id}`,{
+      method:"DELETE",
+      headers:{
+        "Content-type":"application/json; charsert=UTF-8",
+      },
+    }).then((data)=>{
+      alert(`El usuario ${data.first_name} fue eliminado con exit`);
+      tr.remove();
+    })
+  }
 }
